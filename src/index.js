de68ef79d6a129e47265b82b63f723f3ce8c7e5d
alex <sytnikov.zp@gmail.com>
2024-03-23 22:30:38 +0200

    Add getting number function

--- src/index.js
+++ src/index.js
@@ -1,2 +1,34 @@
 'use strict';
 
+const targetValue = Number(prompt('Enter number'));
+
+function gettingNumber(targetValue) {
+  const firstCoef = 3;
+  const secondCoef = 5;
+
+  if (
+    targetValue <= 0 ||
+    targetValue === NaN ||
+    typeof targetValue != 'number'
+  ) {
+    return `Entered wrong data! Please, reload page and try again.`;
+  }
+
+  function findSolution(currentValue = 1, solution = '1') {
+    if (targetValue === currentValue) {
+      return `${targetValue} = ${solution}`;
+    }
+
+    if (currentValue > targetValue) {
+      return null;
+    }
+
+    return (
+      findSolution(currentValue * firstCoef, `(${solution} * ${firstCoef})`) ||
+      findSolution(currentValue + secondCoef, `(${solution} + ${secondCoef})`)
+    );
+  }
+  return findSolution();
+}
+
+console.log(gettingNumber(targetValue));
